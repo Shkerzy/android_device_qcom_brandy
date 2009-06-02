c8105b607b596bebd672b0b217013b0ad7486af2
Naveen Kumar Venkata Siva <c_nvenka@quicinc.com>
2009-06-02 09:57:11 -0700

    vendor/qcom/msm7627_ffa: Enable Bluetooth for msm7627_ffa
    CRs-Fixed: 183783

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -35,7 +35,7 @@ ifeq ($(QC_PROP),true)
     else
     BOARD_GPS_LIBRARIES := libloc_api
     BOARD_CAMERA_LIBRARIES := libcamera
-    #BOARD_HAVE_BLUETOOTH := true
+    BOARD_HAVE_BLUETOOTH := true
     endif   # BUILD_TINY_ANDROID
 
 else
