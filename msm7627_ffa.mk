d8d7956d787ae31743b931c37f65bfe20df7c8f7
Naveen Kumar Venkata Siva <c_nvenka@quicinc.com>
2009-06-04 17:35:29 -0700

    vendor/qcom/msm7627_ffa: Enable Sound notifications for msm7627_ffa
    Sound notifications makefile is included.

--- msm7627_ffa.mk
+++ msm7627_ffa.mk
@@ -5,6 +5,9 @@ PRODUCT_PACKAGES := \
 
 $(call inherit-product, build/target/product/generic_with_google.mk)
 
+#Enabling Ring Tones
+include frameworks/base/data/sounds/OriginalAudio.mk
+
 # Overrides
 PRODUCT_NAME := msm7627_ffa
 PRODUCT_DEVICE := msm7627_ffa
