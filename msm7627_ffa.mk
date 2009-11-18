62b946637e4a319fb3cec6f2606f7a860bc57f1c
Ajay Dudani <adudani@quicinc.com>
2009-11-17 22:52:47 -0800

    msm7627_ffa: Updates for boot up on eclair tree

--- msm7627_ffa.mk
+++ msm7627_ffa.mk
@@ -1,13 +1,13 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/generic.mk)
+#
+# Overrides
+PRODUCT_NAME := msm7627_ffa
+PRODUCT_DEVICE := msm7627_ffa
+
 PRODUCT_PACKAGES := \
     IM \
     VoiceDialer \
     SdkSetup
 
-$(call inherit-product, build/target/product/generic_with_google.mk)
-
 #Enabling Ring Tones
 include frameworks/base/data/sounds/OriginalAudio.mk
-
-# Overrides
-PRODUCT_NAME := msm7627_ffa
-PRODUCT_DEVICE := msm7627_ffa
