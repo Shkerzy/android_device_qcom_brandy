a4ad83c928541862e965f83ca071ed5b73c90422
Gulyuk <gulyuk.sergey@gmail.com>
2012-08-29 20:37:44 +0600

    Product info from build.prop

--- msm7627_ffa.mk
+++ msm7627_ffa.mk
@@ -1,4 +1,14 @@
 $(call inherit-product, device/qcom/common/common.mk)
 
-PRODUCT_NAME := msm7627_ffa
-PRODUCT_DEVICE := msm7627_ffa
+#PRODUCT_BUILD_PROP_OVERRIDES +=  \
+#				PRODUCT_NAME=OneTouch-995 \
+#				BUILD_FINGERPRINT=TCT/one_touch_990_ALRU/one_touch_990_gsm:2.3.4/GINGERBREAD/#v5H6-0:user/release-keys/release-keys \
+#				PRIVATE_BUILD_DESC="one_touch_990_ALRU-user 2.3.4 GINGERBREAD v5H6-0 release-#keys" \
+#				BUILD_NUMBER=v5H6-0
+
+PRODUCT_NAME := one_touch_990_ALRU
+PRODUCT_DEVICE := one_touch_990_gsm
+PRODUCT_MODEL := ALCATEL_one_touch_990
+PRODUCT_BRAND := TCT
+PRODUCT_BOARD := Brandy
+PRODUCT_MANUFACTER := TCT
